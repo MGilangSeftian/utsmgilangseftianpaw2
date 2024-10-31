846362605214076bebbd31891e489e0bd54ebb2b
mgilangseftian <gilangseftian90@gmail.com>
2024-10-31 12:44:04 +0700

    menambahkan link github di router/ mgilangseftian

--- MGilangSeftian/routes/mgilangseftian.js
+++ MGilangSeftian/routes/mgilangseftian.js
@@ -9,4 +9,7 @@ const PariwisataController = require("../controller/mgilangseftian")
 
 router.post("/", PariwisataController.createPariwisata);
 
+// link repository github: 
+// https://github.com/MGilangSeftian/utsmgilangseftianpaw2.git
+
 module.exports = router;
